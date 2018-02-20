7e24f9e350ad5d7424d0d1aba8300932a40e4585
Aluno <Aluno>
2018-02-19 21:36:49 -0300

    criação do javadoc e da pasta src

---
+++ src/aula1PWS20181/HelloGit.java
@@ -0,0 +1,33 @@
+package aula1PWS20181;
+
+/**
+ * Classe Exemplo do uso do Git.
+ * @author Souza.vdj
+ * @version 1.0
+ */
+
+public class HelloGit {
+	
+	/**
+	 * O valor do atributo git é "Hello Git".
+	 */
+	private String git = "Hello Git";
+	
+	/**
+	 * Método de impressão do Hello Git
+	 * @param args parametros de linha de comando da classe
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println(helloGit());
+
+	}
+	
+	/**
+	 * Método que retorna uma String com o valor "Hello Git".
+	 * @return retorna uma String com "Hello Git".
+	 */
+	public static String helloGit () {
+		return "Hello Git";
+	}
+}
